262bf680ee3326fbfa4a0122638040c05943fc75
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-10-01 12:32:49 -0600

    Updated how URLs are generated

--- src/taglibs/docs/DocsFunctions.js
+++ src/taglibs/docs/DocsFunctions.js
@@ -1,56 +1,18 @@
 raptor.define(
     "taglibs.docs.DocsFunctions",
     function(raptor) {
-        var strings = raptor.require('strings');
+        var docsUtil = raptor.require('docs-util');
         
-        var funcs = {
+        return {
             url: function(url) {
-                if (strings.endsWith(url, '.jpg')) {
-                    return funcs.imageUrl.call(this, url);
-                }
-                var basePath = this.basePath;
-                if (!basePath) {
-                    throw raptor.createError(new Error('Base path not set. Use the <shared:urls basePath="<base-path>"/> tag to set the base path for all URLs.'));
-                }
-                var optimizer = raptor.require('optimizer').getOptimizerFromContext(this);
-                var profile = optimizer.getParam('profile');
-                if (profile !== 'production') {
-                    if (basePath === '.') {
-                        if (url === '/') {
-                            return "index.html";
-                        }
-                        else {
-                            url = url.substring(1);    
-                        }
-                        
-                    }
-                    else {
-                        url = basePath + url;
-                    }
-                    
-                    if (strings.endsWith(url, '/')) {
-                        return url + 'index.html';
-                    }
-                    else {
-                        return url + '/index.html';
-                    }
-                }
-                else {
-                    return url === '/' ? url : url + '/';
-                }                
+                return docsUtil.url(url);               
             },
             
             imageUrl: function(url) {
-                var basePath = this.basePath;
-                if (!basePath) {
-                    throw raptor.createError(new Error('Base path not set. Use the <shared:urls basePath="<base-path>"/> tag to set the base path for all URLs.'));
-                }
-                return basePath + '/' + url;         
+                return docsUtil.imageUrl(url); 
             }
             
         };
-        
-        return funcs;
         
     });
     
