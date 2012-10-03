2435d33eb0e73a5d158f7df642f7bc6561591fd4
Patrick Steele-Idem <psteeleidem@ebay.com>
2012-10-03 12:23:36 -0600

    Published documentation

--- src/components/editors/CodeEditor/CodeEditorTag.js
+++ src/components/editors/CodeEditor/CodeEditorTag.js
@@ -39,13 +39,47 @@ raptor.define(
                 if (input.indentUnit) {
                     widgetConfig.indentUnit = input.indentUnit == null ? 4 : input.indentUnit;
                 }
+
+                var bodyText = '';
+
+                if (input.invokeBody) {
+                    bodyText = context.captureString(function() {
+                        input.invokeBody();
+                    }, this);
+                    // console.error('------------');
+                    // console.error(JSON.stringify(bodyText));
+
+                    var initialSpaceMatches = /^\s+/.exec(bodyText);
+                    
+
+                    if (initialSpaceMatches) {
+                        //console.error(JSON.stringify(initialSpaceMatches[0]));
+
+                        var indentMatches = /\n[^\n]*$/.exec(initialSpaceMatches[0]);
+                        if (indentMatches) {
+                            //console.error(JSON.stringify(indentMatches[0]));
+                            var indentRegExp = new RegExp(indentMatches[0].replace(/\n/g, "\\n"), "g");
+                            bodyText = bodyText.replace(indentRegExp, '\n');
+                        }
+
+                        bodyText = bodyText.replace(/^\s*/, '').replace(/\s*$/, '');
+                    }
+                    //console.error(JSON.stringify(bodyText));
+
+                    
+                    
+                }
+
+                
+                
                     
                 raptor.require('templating').render('components/editors/CodeEditor', {
                     tag: input, 
                     widgetConfig: widgetConfig,
                     widgetArgs: input.widgetArgs,
                     textareaAttrs: textareaAttrs,
-                    title: input.title
+                    title: input.title,
+                    bodyText: bodyText
                 }, context);
             }
         };
