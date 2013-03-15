2b2c9adda00c35cda15468404f3dccfc3d849c3a
Patrick Steele-Idem <psteeleidem@ebay.com>
2013-03-15 16:01:02 -0600

    'Published documentation'

--- demos/optimizer-raptor-templates/static/bundle1-908f9109.js
+++
@@ -1,2 +0,0 @@
-define("module-a",function(e){return{sayHello:function(){document.write('<p class="module-a">Hello from "module-a"!</p>')}}});
-define("module-b",function(e){return{sayHello:function(){document.write('<p class="module-b">Hello from "module-b"!</p>')}}});
--- demos/optimizer-raptor-templates/static/index-async-7692ee98.js
+++
@@ -1,1 +0,0 @@
-define("module-d",function(e){return{sayHello:function(){var e=document.createElement("p");e.className="module-d",e.appendChild(document.createTextNode('Hello from "module-d"! (loaded asynchronously)')),document.body.appendChild(e)}}});
